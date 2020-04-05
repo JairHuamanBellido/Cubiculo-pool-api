9e0bb10ecf712a8475b2a6559ee36752c3bef1ea
jair <huamanbellidoj@gmail.com>
2020-04-05 07:47:57 -0700

    Db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_CONFIGURATION } from './config/db.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(DB_CONFIGURATION)],
+
   controllers: [AppController],
   providers: [AppService],
 })
