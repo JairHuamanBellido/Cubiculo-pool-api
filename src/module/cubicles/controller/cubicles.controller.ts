2276d6437fc772ce47b0fae7c1ba0ecafd90c665
jair <huamanbellidoj@gmail.com>
2020-04-19 08:16:15 -0700

    Update type response cubicle controller

--- src/module/cubicles/controller/cubicles.controller.ts
+++ src/module/cubicles/controller/cubicles.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, Res, Query } from '@nestjs/common';
 import { Response } from 'express';
 import { CubiclesService } from '../services/cubicles.service';
 import { ApiResponse, ApiQuery } from '@nestjs/swagger';
+import { CreateCubiculoDTO } from '../dto/create-cubiculos.dto';
 
 @Controller('cubicles')
 export class CubiclesController {
 
     constructor(private cubiclesServices:CubiclesService){}
 
-    @ApiResponse({description: "Retorna todos los cubìculos disponibles",status: 200})
+    @ApiResponse({description: "Retorna todos los cubìculos disponibles",status: 200, type: [CreateCubiculoDTO]})
     @ApiQuery({required:true,description: "Fecha (AAAA-MM-DD)",name: "date"})
     @ApiQuery({required:true, description: "Hora de inicio (hh:00,  07 < hh < 22)", name: "startTime"})
     @ApiQuery({required: true, description: "Horas", name: "hours"})
