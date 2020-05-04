7ab09cc871a49efcd05a1be66a8d5b0acae5e113
jair <huamanbellidoj@gmail.com>
2020-05-04 11:51:57 -0700

    Add algorithm for cron job

--- src/utils/algorithms.ts
+++ src/utils/algorithms.ts
@@ -1,5 +1,34 @@
-export const addPMorAM = (hour)=>{
-    
-    return (hour >= 12) ?  `${hour}:00pm`: 
-                (hour < 10)? `0${hour}:00am`:`${hour}:00am` ;
+import *  as moment from "moment";
+export const calculateDateForCron = (fecha: string, horaInicio: string, minutesBeforeActivation) => {
+
+
+
+    let dayOfWeek = (moment(fecha).weekday());
+
+    let hour = (parseInt(horaInicio.slice(0, 2)) + 5) % 24;
+    let hourString = "";
+    if (hour < 10) {
+        hourString = ("0" + hour).toString()
+    }
+    else {
+        hourString = hour.toString()
+    }
+    let differenceHour = moment(`${fecha}T${hourString}:00:00`)
+
+    let horaAction = (differenceHour.get('hours') - 1)
+
+
+
+    return {
+        dayOfWeek: dayOfWeek,
+
+        horaAction: horaAction,
+        minutos: 40
+    }
+
+}
+export const addPMorAM = (hour) => {
+
+    return (hour >= 12) ? `${hour}:00pm` :
+        (hour < 10) ? `0${hour}:00am` : `${hour}:00am`;
 }
